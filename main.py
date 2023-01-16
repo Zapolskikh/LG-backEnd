add4854134157ce80defb630d3d243a543f701b6
Oleg Zapolskikh <zapolskikh@seznam.cz>
2023-01-16 22:20:17 +0100

    fastapi first step

---
+++ main.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
---
+++ words_game.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+words_table_df = pd.read_excel('D:\LG-backEnd\source\words.xlsx',sheet_name='Sheet1')
+
+# print(words_table_df)
+print(list(words_table_df.iloc[0]))
+
+words_table_df.set_axis(list(words_table_df.iloc[0]),axis=1,inplace=1)
+words_table_df.drop(0,axis=0,inplace=True)
+
+print(words_table_df)
