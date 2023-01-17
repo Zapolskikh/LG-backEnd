81d823f14d83aee7f84bde9e3b47776705a1eb00
Oleg Zapolskikh <zapolskikh@seznam.cz>
2023-01-17 23:48:03 +0100

    added post method, get method from xlsx

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Path
-
+from words_game import get_word
 app = FastAPI()
 
+data = []
 
 word_obj = {
     1: {
@@ -23,3 +24,26 @@ def get_item(value: str):
         if word_obj[item_id]["value"] == value:
             return word_obj[item_id]
     return {"Word": "Not found"}
+
+
+@app.post("/create_item")
+def create_item(language: str, value: str, difficult: str):
+    data.append({
+        "language": language,
+        "value": value,
+        "difficult": difficult
+    })
+ 
+    return {
+        "language": language,
+        "value": value,
+        "difficult": difficult
+    }
+
+@app.get("/data")
+def get_data():
+    return data
+
+@app.get("/get-word")
+def get_word_from_db(word,source_language,dest_language):
+    return get_word(word,source_language,dest_language)
--- words_game.py
+++ words_game.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-words_table_df = pd.read_excel(
-    'words.xlsx', sheet_name='Sheet1')
+def get_word(key,source_language,dest_language):
+    words_table_df = pd.read_excel(
+        'words.xlsx', sheet_name='Sheet1')
 
-# print(words_table_df)
-print(list(words_table_df.iloc[0]))
+    words_table_df = words_table_df.set_axis(list(words_table_df.iloc[0]), axis=1)
+    words_table_df = words_table_df.set_axis(list(words_table_df[source_language.capitalize()]), axis=0)
 
-words_table_df.set_axis(list(words_table_df.iloc[0]), axis=1, inplace=1)
-words_table_df.drop(0, axis=0, inplace=True)
+    # return words_table_df.loc[]
+    return{
+        source_language: key,
+        dest_language: words_table_df.loc[key][dest_language.capitalize()]
+    }
 
-print(words_table_df)
+# get_word('able','english','russian')
